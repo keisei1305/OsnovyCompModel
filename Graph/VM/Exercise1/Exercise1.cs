ec3402dffbcc385011813750879be8207a00fcfe
keisei1305 <kippop88@gmail.com>
2023-11-11 15:49:42 +0300

    Сделал скрытие и показ графиков. Добавил комманды

--- Graph/VM/Exercise1/Exercise1.cs
+++ Graph/VM/Exercise1/Exercise1.cs
@@ -1,17 +1,17 @@
 ﻿using System;
+using Graph.Infrustructure.Commands;
+using Graph.VM;
 using System.Linq;
 using OxyPlot;
 using OxyPlot.Series;
-using Graph.VM.Exercise1;
-using System.Collections.Generic;
 using Graph.Services;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
 
 namespace Graph
 {
-    class Exercise1
+    class Exercise1:ViewModel
     {
         private readonly byte parameterAccuracy = 2;
         private readonly byte tempAccuracy = 4;
@@ -19,12 +19,100 @@ namespace Graph
         private readonly double[] inputY = { 2.4, 2.9, 3.0, 3.5, 3.6, 3.7 };
         private readonly int size = 6;
 
+        private PlotModel _myModel;
+        private bool linearIsHidden = true;
+        private bool powerIsHidden = true;
+        private bool expIsHidden = true;
+        private bool quadricIsHidden = true;
+
         private FunctionSeries linearFunction;
         private FunctionSeries powerFunction;
         private FunctionSeries expFunction;
         private FunctionSeries quadricFunction;
 
-        public PlotModel MyModel { get; private set; }
+        public PlotModel MyModel
+        {
+            get => _myModel;
+            private set
+            {
+                _myModel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #region Commands
+        public ICommand ShowLinearCommand { get; }
+        private bool CanShowLinearCommandExecute(object p) => true;
+        private void OnShowLinearCommandExecuted(object p)
+        {
+            if (linearIsHidden)
+            {
+                MyModel.Series.Remove(linearFunction);
+                MyModel.InvalidatePlot(true);
+            }
+            else
+            {
+                MyModel.Series.Add(linearFunction);
+                MyModel.InvalidatePlot(true);
+            }
+            linearIsHidden = !linearIsHidden;
+        }
+        public ICommand ShowPowerCommand { get; }
+        private bool CanShowPowerCommandExecute(object p) => true;
+        private void OnShowPowerCommandExecuted(object p)
+        {
+            if (powerIsHidden)
+            {
+                MyModel.Series.Remove(powerFunction);
+                MyModel.InvalidatePlot(true);
+            }
+            else
+            {
+                MyModel.Series.Add(powerFunction);
+                MyModel.InvalidatePlot(true);
+            }
+            powerIsHidden = !powerIsHidden;
+        }
+        public ICommand ShowExpCommand { get; }
+        private bool CanShowExpCommandExecute(object p) => true;
+        private void OnShowExpCommandExecuted(object p)
+        {
+            if (expIsHidden)
+            {
+                MyModel.Series.Remove(expFunction);
+                MyModel.InvalidatePlot(true);
+            }
+            else
+            {
+                MyModel.Series.Add(expFunction);
+                MyModel.InvalidatePlot(true);
+            }
+            expIsHidden = !expIsHidden;
+        }
+        public ICommand ShowQuadricCommand { get; }
+        private bool CanShowQuadricCommandExecute(object p) => true;
+        private void OnShowQuadricCommandExecuted(object p)
+        {
+            if (quadricIsHidden)
+            {
+                MyModel.Series.Remove(quadricFunction);
+                MyModel.InvalidatePlot(true);
+            }
+            else
+            {
+                MyModel.Series.Add(quadricFunction);
+                MyModel.InvalidatePlot(true);
+            }
+            quadricIsHidden = !quadricIsHidden;
+        }
+
+        public ICommand UpdateModel { get; }
+        private bool CanUpdateModelExecute(object p) => true;
+        private void OnUpdateModelExecuted(object p)
+        {
+
+        }
+        #endregion
 
         private Matrix MakeSystem(double[] listX, double[] listY, int dim)
         {
@@ -89,6 +177,11 @@ namespace Graph
 
         public Exercise1()
         {
+            ShowLinearCommand = new LambdaCommand(OnShowLinearCommandExecuted,CanShowLinearCommandExecute);
+            ShowPowerCommand = new LambdaCommand(OnShowPowerCommandExecuted, CanShowPowerCommandExecute);
+            ShowExpCommand = new LambdaCommand(OnShowExpCommandExecuted, CanShowExpCommandExecute);
+            ShowQuadricCommand = new LambdaCommand(OnShowQuadricCommandExecuted, CanShowQuadricCommandExecute);
+
             var coefs = MathMethods.MethodKramera(MakeSystem(inputX, inputY, 2));
             MyModel = new PlotModel { Title = "Апроксимация экспериментальных данных методом наименьших квадратов" };
             var points = new LineSeries
---
+++ Graph/VM/ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Graph.VM
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
