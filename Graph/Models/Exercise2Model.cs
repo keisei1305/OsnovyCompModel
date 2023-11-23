dd6d3bcd9494df1d1f8c641bd4776f08f44372c5
keisei1305 <kippop88@gmail.com>
2023-11-23 19:04:22 +0300

    Добавил пофреймовое переключение моделей

---
+++ Graph/Models/Exercise2Model.cs
@@ -0,0 +1,25 @@
+﻿using OxyPlot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph.Models
+{
+    public class Exercise2Model
+    {
+        static public List<PlotModel> GetModels()
+        {
+            var models = new List<PlotModel>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                models.Add(new PlotModel { Title = $"{i}" });
+            }
+
+
+            return models;
+        }
+    }
+}
---
+++ Graph/VM/Exercise2/Exercise2VM.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Graph.VM;
+using Graph.Models;
+using OxyPlot;
+using Graph.Infrustructure.Commands;
+
+namespace Graph.VM.Exercise2
+{
+    public class Exercise2VM : ViewModel
+    {
+        List<PlotModel> models;
+        PlotModel _currentModel;
+        public PlotModel CurrentModel
+        {
+            get => _currentModel;
+            set
+            {
+                _currentModel = value;
+                OnPropertyChanged();
+            }
+        }
+        byte currentIndex = 0;
+        string _absoluteError;
+        string _relativeError;
+        short _pointsQuantity;
+        public string RelativeError
+        {
+            get => _relativeError;
+            set
+            {
+                _relativeError = value;
+                OnPropertyChanged();
+            }
+        }
+        public string AbsoluteError
+        {
+            get => _absoluteError;
+            set
+            {
+                _absoluteError = value;
+                OnPropertyChanged();
+            }
+        }
+        public short PointsQuantity
+        {
+            get => _pointsQuantity;
+            set
+            {
+                _pointsQuantity = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand NextModelCommand { get; }
+        private bool CanNextModelCommandExecute(object p) => currentIndex < 3;
+        private void OnNextModelCommandExecuted(object p)
+        {
+            currentIndex++;
+            CurrentModel = models[currentIndex];
+        }
+
+        public ICommand PreviousModelCommand { get; }
+        private bool CanPreviousModelCommandExecute(object p) => currentIndex > 0;
+        private void OnPreviousModelCommandExecuted(object p)
+        {
+            currentIndex--;
+            CurrentModel = models[currentIndex];
+        }
+
+        public Exercise2VM()
+        {
+            NextModelCommand = new LambdaCommand(OnNextModelCommandExecuted, CanNextModelCommandExecute);
+            PreviousModelCommand = new LambdaCommand(OnPreviousModelCommandExecuted, CanPreviousModelCommandExecute);
+            models = Exercise2Model.GetModels();
+            CurrentModel = models[0];
+        }
+    }
+}
