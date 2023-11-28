992fcce44cacf38644bb3cae3a2d9dc19904d261
keisei1305 <kippop88@gmail.com>
2023-11-28 13:25:19 +0300

    Сделал мат методы для 3го задания

--- Graph/Models/Exercise3Model.cs
+++ Graph/Models/Exercise3Model.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Graph.Services;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,15 +9,68 @@ namespace Graph.Models
 {
     public class Exercise3Model
     {
-        private double Neimon(double p)
+        static private double GetMiddleNum(double p)
         {
-            p = Math.Pow(p, 2);
-            p *= 100;
-            for(int i=0; i<4; i++)
+            var s = p.ToString();
+            if (s.Length < 8)
             {
-
+                while (s.Length != 10)
+                {
+                    s += "0";
+                }
             }
-            return 0;
+            s = s.Substring(4,4);
+            return Convert.ToDouble(s);
+        }
+        static private IEnumerable<double> Neimon(double p, int n)
+        {
+            double previousNum = p;
+            yield return previousNum;
+            for(int i=0; i<n; i++)
+            {
+                previousNum = GetMiddleNum(Math.Pow(previousNum,2))/10000;
+                yield return previousNum;
+            }
+        }
+
+        static private IEnumerable<double> ModifiedNeimon(double p1,double p2, int n)
+        {
+            double result;
+            for(int i=0; i <= n; i++)
+            {
+                result = GetMiddleNum(p1 * p2)/10000;
+                yield return result;
+                p1 = p2;
+                p2 = result;
+            }
+        }
+
+        static private IEnumerable<double> Lemer(double p,double g, int n)
+        {
+            double result = p;
+            for(int i=0; i<n; i++)
+            {
+                result = g * result % 1;
+                yield return Math.Round(result,4);
+            }
+        }
+
+        static private IEnumerable<double> CongruantMultLemer(int a, int m, int p,int n)
+        {
+            double result = p;
+            for(int i=0; i<n; i++)
+            {
+                result = a * result % m;
+                yield return Math.Round(result / m,4);
+            }
+        }
+
+        static public IEnumerable<IEnumerable<double>> GetMatrix()
+        {
+            yield return Neimon(0.5830, 6);
+            yield return ModifiedNeimon(0.5836, 0.2176, 6);
+            yield return Lemer(0.585, 927, 5);
+            yield return CongruantMultLemer(265, 129, 122, 4);
         }
     }
 }
--- Graph/VM/Exercise3/Exercise3VM.cs
+++ Graph/VM/Exercise3/Exercise3VM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,31 @@ namespace Graph.VM.Exercise3
 {
     public class Exercise3VM:ViewModel
     {
+        public DataTable Table { get; set; }
+        public void InitTable(IEnumerable<IEnumerable<double>> list)
+        {
+            Table = new DataTable();
+            Table.Columns.Add(new DataColumn("Имя метода"));
+            for(int i=0; i<7; i++)
+            {
+                Table.Columns.Add(new DataColumn($"{i+1}"));
+            }
+            foreach(IEnumerable<double> itemList in list)
+            {
+                var row = Table.NewRow();
+                int i = 1;
+                foreach(double item in itemList)
+                {
+                    row[i] = item;
+                    i++;
+                }
+                Table.Rows.Add(row);
+            }
+        }
         public Exercise3VM()
         {
-
+            IEnumerable<IEnumerable<double>> list = Graph.Models.Exercise3Model.GetMatrix();
+            InitTable(list);
         }
     }
 }
